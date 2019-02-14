49c1988b119e7ceeb09c5c1ba5ac3ccd1a6cce9d
Filippo Guida <filippo.guida@icloud.com>
2019-02-14 11:36:27 +0100

    Update 14022019.py

--- 9b/14022019.py
+++ 9b/14022019.py
@@ -1,11 +1,13 @@
 incoming_numbers = [1, 345, 20, 34, 234]
 
 #inside
+print("Numbers inside the set")
 for num in incoming_numbers:
   if num >= -23 and num < 57:
     print(num)
 
 #outside
+print("Numbers outside the set")
 for num in incoming_numbers:
   if num > 57 or num <= -23:
     print(num)
