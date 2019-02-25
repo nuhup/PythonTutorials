8148ef11fefdf7fad6375a74456da1e090586468
Filippo Guida <filippo.guida@icloud.com>
2019-02-25 11:17:43 +0100

    Create 20190215.py

---
+++ 8b/20190215.py
@@ -0,0 +1,19 @@
+#Automatic Test Review
+
+#Simple variable
+threshold = 15 #min result to pass
+
+#Complex Variable - Dictionary
+#This data-structure is called dictionary.
+#Allows us to store multiple datas in
+#one single variable.
+student = {
+  "result": 18,
+  "name": "Tom",
+  "surname": "Cruise"
+}
+
+if student["result"] >= threshold:
+  print(student["name"], student["surname"], "passed")
+else:
+  print(student["name"], student["surname"], "failed")
