f7d7d7e4455b6cf4152ff1d86cc1e205957ab1df
Filippo Guida <filippo.guida@icloud.com>
2019-02-25 11:47:16 +0100

    Update 20190215.py

--- 8b/20190215.py
+++ 8b/20190215.py
@@ -5,8 +5,7 @@ threshold = 15 #min result to pass
 
 #Complex Variable - Dictionary
 #This data-structure is called dictionary.
-#Allows us to store multiple datas in
-#one single variable.
+#Allows us to store multiple data in one single variable.
 student = {
   "result": 18,
   "name": "Tom",
