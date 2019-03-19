d2a64c7903026bb3f32469a9eadb6ca2daf6bad9
Filippo Guida <filippo.guida@icloud.com>
2019-03-19 11:56:33 +0100

    Create 20190319.py

---
+++ 9a/20190319.py
@@ -0,0 +1,26 @@
+#Starting from this counter + decimal shift implementation:
+a = a * 10
+a = a + 2
+
+a = a * 10
+a = a + 3
+
+a = a * 10 
+a = a + 4
+
+a = a * 10
+a = a + 5
+
+#so on until N
+
+print(a)
+
+
+#Solve this exercise - From repetitions to  loop
+n = 10
+count = 1
+while count < n:
+  
+  #write code here
+
+  count = count + 1
