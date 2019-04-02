f9e40a4c6b3c3c1e6a0dac5d2e398e1133ebb5a9
Filippo Guida <filippo.guida@icloud.com>
2019-04-02 11:44:29 +0200

    Create home_assignments.py

---
+++ 8b/home_assignments.py
@@ -0,0 +1,46 @@
+#Exercise 1 (2 points)
+#Evaluate Sum and Average for this collection of incoming numbers 
+#HINT: remember that the funciton len(c) provides the length of the collection 'c'
+#NOTE: print them in console
+
+inc_num = [100, 20, 20, 30, 30, 50, 50]
+
+
+#Exercise 2 (2 points)
+#The following data-structure represent a police officers in the Mitte department. 
+#Inside the officer dictionary there is a list representing the value of each fine
+#that the police officer made today (in euro).
+#Evaluate the Sum and the Average for those tickets
+#NOTE: print in console Name, Surname, ID, Sum and ticket (individual lines is fine)
+
+police_officer = {
+  "tickets_values": [100, 20, 20, 30, 30, 50, 50],
+  "name": "Magnum",
+  "surname": "P.I.",
+  "id": 70859
+}
+
+
+#Exercise 3 (1 points - to get 1)
+#Apply the solution in Ex num 2, for a list of police officers, like the following one.
+
+police_officers = [
+  {
+    "tickets_values": [100, 20, 20, 30, 30, 50, 50],
+    "name": "Vincent",
+    "surname": "Vega",
+    "id": 70859
+  },
+   {
+    "tickets_values": [43, 52, 63, 78, 66],
+    "name": "Jules",
+    "surname": "Winnfield",
+    "id": 64738
+  },
+   {
+    "tickets_values": [30, 20],
+    "name": "Mia",
+    "surname": "Wallace",
+    "id": 33984
+  } # so on...
+]
